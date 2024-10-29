4cdb5d0176e59df758c535da976bc53d93d988b1
Parsa <deadlockfree504@gmail.com>
2024-10-29 17:00:52 +0330

    added model UserAccount

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
-class accounts(models.Model):
+class UserAccounts(models.Model):
     username = models.CharField(max_length=256)
-    models.CharField(max_length=256)
+    password = models.CharField(max_length=256)
+    email = models.CharField(max_length=256)
+    creationDate = models.DateTimeField(auto_now_add=True)
+    #IDK WHAT THE FUCK IM DOING ATP ;/ help!
+    def __str__(self):
+        return f"USERNAME : {self.username} Created At {self.creationDate}"
